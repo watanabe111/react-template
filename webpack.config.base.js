bbe18bc178e552c2bc5c57f45d800d362adf4448
s.watanabe <s.watanabe.devel@gmail.com>
2019-08-28 19:23:04 +0900

    add css

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,6 +16,11 @@ module.exports = {
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"]
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
       }
     ]
   },
